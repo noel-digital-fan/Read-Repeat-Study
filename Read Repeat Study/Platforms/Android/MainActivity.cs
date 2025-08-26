fa40373d7d1eff97b9c4ab4731472a97acb88482
Noel <ndolz1@wgu.edu>
2025-08-25 22:31:06 -0400

    FIX Crash in Release mode

--- Read Repeat Study/Platforms/Android/MainActivity.cs
+++ Read Repeat Study/Platforms/Android/MainActivity.cs
@@ -1,6 +1,9 @@
 ﻿using Android.App;
 using Android.Content.PM;
+using Android.OS;
+using Android.Util;
 using Microsoft.Maui;
+using System.Text;
 
 namespace Read_Repeat_Study
 {
@@ -10,6 +13,7 @@ namespace Read_Repeat_Study
         RoundIcon = "@mipmap/appicon_round",
         Theme = "@style/Maui.SplashTheme",
         MainLauncher = true,
+        Exported = true,
         ConfigurationChanges =
           ConfigChanges.ScreenSize |
           ConfigChanges.Orientation |
@@ -18,5 +22,30 @@ namespace Read_Repeat_Study
           ConfigChanges.SmallestScreenSize)]
     public class MainActivity : MauiAppCompatActivity
     {
+        protected override void OnCreate(Bundle? savedInstanceState)
+        {
+            try
+            {
+                base.OnCreate(savedInstanceState);
+                Log.Debug("RRS", "MainActivity OnCreate OK (release) ");
+                AppendDiag("MainActivity OnCreate reached.\n");
+            }
+            catch (System.Exception ex)
+            {
+                Log.Error("RRS", "MainActivity crash: " + ex);
+                AppendDiag("MainActivity crash: " + ex + "\n");
+                throw;
+            }
+        }
+
+        void AppendDiag(string text)
+        {
+            try
+            {
+                var path = System.IO.Path.Combine(FileSystem.AppDataDirectory, "crash.log");
+                System.IO.File.AppendAllText(path, System.DateTime.UtcNow.ToString("u") + " " + text);
+            }
+            catch { }
+        }
     }
 }
--- Read Repeat Study/Platforms/Android/MainApplication.cs
+++ Read Repeat Study/Platforms/Android/MainApplication.cs
@@ -1,5 +1,6 @@
 ﻿using Android.App;
 using Android.Runtime;
+using System.Text;
 
 namespace Read_Repeat_Study
 {
@@ -9,6 +10,51 @@ namespace Read_Repeat_Study
         public MainApplication(IntPtr handle, JniHandleOwnership ownership)
             : base(handle, ownership)
         {
+        }
+
+        public override void OnCreate()
+        {
+            base.OnCreate();
+            try
+            {
+                // Register global handlers so we can capture the actual crash in Release
+                AndroidEnvironment.UnhandledExceptionRaiser += (s, e) =>
+                {
+                    try { LogCrash("AndroidEnvironment", e.Exception); } catch { }
+                };
+                AppDomain.CurrentDomain.UnhandledException += (s, e) =>
+                {
+                    try { LogCrash("AppDomain", e.ExceptionObject as Exception); } catch { }
+                };
+                TaskScheduler.UnobservedTaskException += (s, e) =>
+                {
+                    try { LogCrash("TaskScheduler", e.Exception); } catch { }
+                };
+            }
+            catch { /* swallow – never block app start */ }
+        }
+
+        static void LogCrash(string source, Exception? ex)
+        {
+            if (ex == null) return;
+            var sb = new StringBuilder();
+            sb.AppendLine(DateTime.UtcNow.ToString("u"));
+            sb.AppendLine($"SOURCE: {source}");
+            while (ex != null)
+            {
+                sb.AppendLine(ex.GetType().FullName);
+                sb.AppendLine(ex.Message);
+                sb.AppendLine(ex.StackTrace);
+                ex = ex.InnerException;
+                if (ex != null) sb.AppendLine("-- Inner Exception --");
+            }
+            sb.AppendLine(new string('-', 60));
+            try
+            {
+                var path = Path.Combine(FileSystem.AppDataDirectory, "crash.log");
+                File.AppendAllText(path, sb.ToString());
+            }
+            catch { }
         }
 
         protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
